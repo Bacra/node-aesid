02be3b163737566aea8cadad34512bac7bb467ee
bacrawu <bacrawu@tencent.com>
2019-10-02 21:43:13 +0800

    Add: version test

--- test/test_base.js
+++ test/test_base.js
@@ -17,6 +17,28 @@ describe('#base', () => {
 		expect(sidAes.decrypt('test', sid)).to.be(content);
 	});
 
+	it('#version', () => {
+		const sidOptions = {
+			business: {
+				test: {
+					1: 'test 123',
+					2: 'test 1234',
+					last: 1
+				}
+			}
+		};
+
+		const sidAes1 = aesid(sidOptions);
+		sidOptions.business.test.last = 2;
+		const sidAes2 = aesid(sidOptions);
+
+		const content = 'test content';
+		const sid = sidAes1.encrypt('test', content);
+
+		expect(sidAes1.decrypt('test', sid)).to.be(content);
+		expect(sidAes2.decrypt('test', sid)).to.be(content);
+	});
+
 	describe('#userid', () => {
 		const content = 'test content';
 		const sidAes = aesid({
@@ -28,17 +50,17 @@ describe('#base', () => {
 			}
 		});
 
-		it('#string', function() {
+		it('#string', () => {
 			const sid = sidAes.encrypt('test', content, 'userid123');
 			expect(sidAes.decrypt('test', sid, 'userid123')).to.be(content);
 		});
 
-		it('#number', function() {
+		it('#number', () => {
 			const sid = sidAes.encrypt('test', content, 1234);
 			expect(sidAes.decrypt('test', sid, 1234)).to.be(content);
 		});
 
-		it('#buffer', function() {
+		it('#buffer', () => {
 			const useridBuf = Buffer.from('userid');
 			const sid = sidAes.encrypt('test', content, useridBuf);
 			expect(sidAes.decrypt('test', sid, useridBuf)).to.be(content);
